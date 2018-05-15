9a4515bf210adc910dbdf8db214fc0c9ad12dab6
Wator Vapor <watorvapor@gmail.com>
2018-05-15 21:38:06 +0900

    Update ja.js

---
+++ nodejs_sample/ja.js
@@ -0,0 +1,11 @@
+const level = require('level');
+let dbPath = '../leveldb/cnwiktionary';
+let db = level(dbPath);
+let word = args.targets[0];
+db.get(word, function (err, value) {
+  if (err && err.notFound) {
+    console.log('word err=<',err,'>');
+  } else {
+    console.log('word=<',word,'>',value=<',value,'>');
+  }
+});
