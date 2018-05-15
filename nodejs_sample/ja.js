557ae0bc564d31e278f353c3aedba9f8027d9056
Wator Vapor <watorvapor@gmail.com>
2018-05-15 21:41:44 +0900

    Update ja.js

--- nodejs_sample/ja.js
+++ nodejs_sample/ja.js
@@ -1,7 +1,11 @@
 const level = require('level');
+const argv = require('argv');
+
 let dbPath = '../leveldb/cnwiktionary';
 let db = level(dbPath);
+let args = argv.run();
 let word = args.targets[0];
+
 db.get(word, function (err, value) {
   if (err && err.notFound) {
     console.log('word err=<',err,'>');
